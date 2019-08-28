437b985981c62abedff20b65f731de32e1836b92
Emily Lillie <emilylillie3@gmail.com>
2019-08-28 23:09:29 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,4 @@
+#load environment and get tasks from sinatr-activerecord gem
+
+require './config/environment'
+require 'sinatra/activerecord/rake'
---
+++ db/migrate/20190828230338_create_dogs.rb
@@ -0,0 +1,21 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def up
+   create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end 
+  end
+ 
+  def down
+   drop_table :dogs 
+  end
+  
+  # or make it shorter with 'change' bc the down method is included in the rollback 
+  # def change
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  
+end
